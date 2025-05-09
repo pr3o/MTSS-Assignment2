d3755d7aa7182510d2639026cee769446d054aa0
pr3o <158464877+pr3o@users.noreply.github.com>
2025-05-09 11:24:17 +0200

    Setup Progetto con Checkstyle e file di base

---
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -0,0 +1,15 @@
+package it.unipd.mtss;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class IntegerToRomanTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
